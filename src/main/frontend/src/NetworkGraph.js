953eaa484b956a01881aa86c6c79e9bd3ba78d62
jessy <jessy-g92@hotmail.com>
2021-01-05 20:56:42 -0500

    NetworkGraph using ReactFlow

--- src/main/frontend/src/NetworkGraph.js
+++ src/main/frontend/src/NetworkGraph.js
@@ -1,108 +1,141 @@
-import { Fragment } from "react";
-import { Graph } from "react-d3-graph";
+import React from 'react';
+import ReactFlow from 'react-flow-renderer';
+import './NetworkGraph.css';
 
-// graph payload (with minimalist structure)
-const data = {
-    nodes: [{ id: "A" }, { id: "B" }, { id: "C" }],
-    links: [
-      { source: "A", target: "B" },
-      { source: "A", target: "C" },
+const elements = [
+  {
+    id: '1',
+    sourcePosition: 'right',
+    type: 'input',
+    data: { label: 'A' },
+    position: { x: 0, y: 80 },
+    connectable : false
+  },
+  {
+    id: '2',
+    sourcePosition: 'right',
+    targetPosition: 'left',
+    data: { label: 'B' },
+    position: { x: 250, y: 0 },
+    connectable : false
+  },
+  {
+    id: '3',
+    sourcePosition: 'right',
+    targetPosition: 'left',
+    data: { label: 'C' },
+    position: { x: 250, y: 160 },
+    connectable : false
+  },
+  {
+    id: '4',
+    sourcePosition: 'right',
+    targetPosition: 'left',
+    data: { label: 'D' },
+    position: { x: 500, y: 0 },
+    connectable : false
+  },
+  {
+    id: '5',
+    sourcePosition: 'top',
+    targetPosition: 'bottom',
+    data: { label: 'E' },
+    position: { x: 400, y: 90 },
+    connectable : false
+  },
+  {
+    id: '6',
+    sourcePosition: 'bottom',
+    targetPosition: 'top',
+    data: { label: 'F' },
+    position: { x: 500, y: 230 },
+    connectable : false
+  },
+  {
+    id: '7',
+    sourcePosition: 'right',
+    targetPosition: 'left',
+    data: { label: 'G' },
+    position: { x: 750, y: 50 },
+    connectable : false
 
-    ],
-  };
+  },
+  {
+    id: '8',
+    sourcePosition: 'right',
+    targetPosition: 'left',
+    data: { label: 'H' },
+    position: { x: 750, y: 300 },
+    connectable : false
+  },
+  {
+    id: 'e1-2',
+    source: '1',
+    type: 'smoothstep',
+    target: '2',
+    animated: true,
+  },
+  {
+    id: 'e1-3',
+    source: '1',
+    type: 'smoothstep',
+    target: '3',
+    animated: true,
+  },
+  {
+    id: 'e1-5',
+    source: '1',
+    type: 'smoothstep',
+    target: '5',
+    animated: true,
+  },
+  {
+    id: 'e2-4',
+    source: '2',
+    type: 'smoothstep',
+    target: '4',
+    label: 'edge label',
+    animated: true
+  },
+  {
+    id: 'e3-5',
+    source: '3',
+    type: 'smoothstep',
+    target: '5',
+    animated: true,
+  },
+  {
+    id: 'e3-6',
+    source: '3',
+    type: 'smoothstep',
+    target: '6',
+    animated: true,
+  },
+  {
+    id: 'e5-7',
+    source: '5',
+    type: 'smoothstep',
+    target: '7',
+    animated: true,
+  },
+  {
+    id: 'e6-8',
+    source: '6',
+    type: 'smoothstep',
+    target: '8',
+    animated: true,
+  },
+];
 
-  // the graph configuration, just override the ones you need
-  const myConfig = {
-    "automaticRearrangeAfterDropNode": false,
-    "collapsible": false,
-    "directed": false,
-    "focusAnimationDuration": 0,
-    "focusZoom": 0,
-    "freezeAllDragEvents": true,
-    "height": 400,
-    "highlightDegree": 1,
-    "highlightOpacity": 1,
-    "linkHighlightBehavior": false,
-    "maxZoom": 0,
-    "minZoom": 0,
-    "nodeHighlightBehavior": false,
-    "panAndZoom": false,
-    "staticGraph": false,
-    "staticGraphWithDragAndDrop": false,
-    "width": 800,
-    "d3": {
-      "alphaTarget": 0.05,
-      "gravity": -300,
-      "linkLength": 100,
-      "linkStrength": 1,
-      "disableLinkForce": false
-    },
-    "node": {
-      "color": "#d3d3d3",
-      "fontColor": "black",
-      "fontSize": 18,
-      "fontWeight": "normal",
-      "highlightColor": "SAME",
-      "highlightFontSize": 8,
-      "highlightFontWeight": "normal",
-      "highlightStrokeColor": "SAME",
-      "highlightStrokeWidth": "SAME",
-      "labelProperty": "id",
-      "mouseCursor": "mouse",
-      "opacity": 1,
-      "renderLabel": true,
-      "size": 500,
-      "strokeColor": "black",
-      "strokeWidth": 1,
-      "svg": "",
-      "symbolType": "circle"
-    },
-    "link": {
-      "color": "black",
-      "fontColor": "red",
-      "fontSize": 20,
-      "fontWeight": "normal",
-      "highlightColor": "black",
-      "highlightFontSize": 8,
-      "highlightFontWeight": "normal",
-      "labelProperty": ran,
-      "mouseCursor": "mouse",
-      "opacity": 1,
-      "renderLabel": true,
-      "semanticStrokeWidth": false,
-      "strokeWidth": 1,
-      "markerHeight": 6,
-      "markerWidth": 6,
-      "strokeDasharray": 0,
-      "strokeDashoffset": 0,
-      "strokeLinecap": "butt"
-    }
-  };
+  // function ran() {
+  //   return "" + Math.ceil(Math.random() * (10 - 1) + 1);
+  // }
 
-  function ran() {
-    return Math.ceil(Math.random() * (10 - 1) + 1);
-  }
-  // return Math.ceil(Math.random() * (10 - 1) + 1);
-  
-  const onClickNode = function(nodeId) {
-    window.alert(`Clicked node ${nodeId}`);
-  };
-  
-  const onClickLink = function(source, target) {
-    window.alert(`Clicked link between ${source} and ${target}`);
-  };
-  
-  function NetworkGraph() {
-    return (
-        <Fragment>
-            <Graph
-                id="graph-id" // id is mandatory
-                data={data}
-                config={myConfig}
-                onClickNode={onClickNode}
-                onClickLink={onClickLink}
-            />
-        </Fragment>
-    )
-  }
+function NetworkGraph() {
+  return (
+    <div style={{ height: 400 }}>
+      <ReactFlow elements={elements} />
+  </div>
+  );
+}
 export default NetworkGraph
